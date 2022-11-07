06e97fb43f87f8def3dc29a3aa0a4223e84ddc70
andrew <statco19@gmail.com>
2022-11-07 13:27:14 +0900

    string,sorting 800

---
+++ 339A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
+
+void sol() {
+	string s;
+	cin >> s;
+	vt<string> v = split(s,'+');
+	vt<int> res;
+	for(int i=0; i<sz(v); ++i) {
+		res.pb(stoi(v[i]));
+	}
+	sort(all(res));
+	for(int i=0; i<sz(res); ++i) {
+		cout << res[i];
+		if(i != sz(res)-1) cout << '+';
+	}
+	cout << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
