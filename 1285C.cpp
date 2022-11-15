cc4480c9fc5d3f7d6ca46e02cd8fd3c080fb5e1e
andrew <statco19@gmail.com>
2022-11-15 12:52:57 +0900

    number theory 1400

---
+++ 1285C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+ll gcd(ll a, ll b) {
+	return b ? gcd(b,a%b) : a;
+}
+
+void sol() {
+	ll x;
+	cin >> x;
+
+	ll k = 1, l = x;
+	for(ll i=2; i<=(ll)sqrt(x); ++i) {
+		if(x % i == 0 && x / gcd(i,x/i) == x) {
+			k = i;
+			l = x / i;
+		}
+	}
+	cout << k << ' ' << l << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
