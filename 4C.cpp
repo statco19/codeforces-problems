ed0ce5326e204cbbef4595e08949cb359e5e7252
andrew <statco19@gmail.com>
2022-11-27 20:08:30 +0900

    hashing 1300

---
+++ 4C.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+int n;
+umap<string,int> um;
+
+void sol() {
+	cin >> n;
+	string name;
+	for(int i=0; i<n; ++i) {
+		cin >> name;
+		if(um.find(name) == um.end()) {
+			um.ioa(name,1);
+			cout << "OK" << en;
+		} else {
+			auto p = um.find(name);
+			cout << name << p->second << en;
+			um.ioa(name, (p->second)+1);
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
