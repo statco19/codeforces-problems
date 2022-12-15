bb5535351976cafd4a656e5af6824e5e698037b2
andrew <statco19@gmail.com>
2022-12-15 18:17:58 +0900

    bitmask, impl 1300 *

---
+++ 611B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+
+void sol() {
+	// Reference
+	// https://ideone.com/Kfu8sF
+	// https://codeforces.com/blog/entry/22441
+	ll a, b;
+	ll cnt = 0;
+	cin >> a >> b;
+
+	for(int i=0; (1LL<<i) / 2 <=b; ++i) {
+		for(int j=0; j<=i-2; ++j) {
+			ll x = (1LL<<i) - 1 - (1LL<<j);
+			cnt += a<=x && x<=b;
+		}
+	}
+	cout << cnt << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
