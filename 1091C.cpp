ac9b8a36577e1052cbdfc7214996a2827e23f492
andrew <statco19@gmail.com>
2022-12-18 18:54:48 +0900

    number theory 1400 *

---
+++ 1091C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+
+void sol() {
+	ll n;
+	cin >> n;
+
+	vt<ll> v;
+	for(ll i=1; i*i<=n; ++i) {
+		if(n%i == 0) {
+			v.pb(n*(i-1)/2 + i);
+			if(i*i != n) {
+				v.pb(n*(n/i-1)/2 + n/i);
+			}
+		}
+	}
+	sort(all(v));
+
+	for(ll x : v) {
+		cout << x << ' ';
+	}
+	cout << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
