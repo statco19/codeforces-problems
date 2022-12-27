6065806e6421ae15202b0d88b3562c962d100f6c
andrew <statco19@gmail.com>
2022-12-27 17:01:15 +0900

    greedy, counter by prefix sum 1500 *

---
+++ 276C-1.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en '\n'
+#define mid (st+end)/2
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n,q;
+	cin >> n >> q;
+	int a[200001] = {0}, b[200001] = {0}, d[200001] = {0};
+
+	for(int i=1; i<=n; ++i) {
+		cin >> a[i];
+	}
+	sort(a+1, a+n+1);
+
+	int l,r;
+
+	// counter using prefix sum
+	for(int i=0; i<q; ++i) {
+		cin >> l >> r;
+		d[l]++;
+		if(r < n) d[r+1]--;
+	}
+
+	int v = 0;
+	for(int i=1; i<=n; ++i) {
+		v += d[i];
+		b[i] = v;
+	}
+
+	sort(b+1, b+n+1);
+	ll ans = 0;
+
+	for(int i=1; i<=n; ++i) {
+		ans += 1LL * a[i] * b[i];
+	}
+
+	cout << ans << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
