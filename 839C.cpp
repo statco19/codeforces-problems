32f89994d6f0ac3677e7a61d3b1ec2dbea28886d
andrew <statco19@gmail.com>
2023-01-11 20:12:35 +0900

    dfs, tree 1500 *

---
+++ 839C.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+int n;
+vt<int> adj[100000];
+
+double dfs(int v = 0, int p = -1) {
+	double sum = 0;
+	for(int u : adj[v]) {
+		if(u != p) {
+			sum += dfs(u, v) + 1;
+		}
+	}
+
+	return sum ? sum / (sz(adj[v]) - (p != -1)) : 0;
+}
+
+void sol() {
+	cin >> n;
+	int u,v;
+	for(int i=0; i<n-1; ++i) {
+		cin >> u >> v, u--, v--;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+
+	cout << fixed << setprecision(7) << dfs() << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
