c031f4f1f7e6980f96d246f3f56eef5e183e54c1
andrew <statco19@gmail.com>
2023-01-20 11:43:15 +0900

    constructive, math 800

---
+++ 1774A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en "\n"
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n;
+	string s;
+	cin >> n >> s;
+	int sum = 0;
+
+	if(s[0]-'0' == 1) sum++;
+	for(int i=1; i<sz(s); ++i) {
+		if(s[i] == '0') cout << '+';
+		else {
+			if(sum) {
+				if(s[i] == '1') {
+					cout  << '-';
+					sum--;
+				} else {
+					cout << '+';
+				}
+			} else {
+				if(s[i] == '1') {
+					cout << '+';
+					sum++;
+				} else {
+					cout << '+';
+				}
+			}
+		}
+	}
+	cout << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> T;
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
