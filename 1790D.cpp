e94edec1cbbafa49d6381b9f836a85eae4d938e9
andrew <statco19@gmail.com>
2023-02-01 14:27:42 +0900

    data structures, greedy, sortings

---
+++ 1790D.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en "\n"
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n;
+	cin >> n;
+	vt<int> v(n);
+	for(int &x : v) cin >> x;
+	
+	map<int,int> mp;
+	for(int x : v) {
+		mp[x]++;
+	}
+
+	int ans = 0;
+	auto it = mp.begin();
+	int size = mp.begin()->first;
+	if(--mp[size] == 0)
+		mp.erase(size);
+
+	while(!mp.empty()) {
+		if(mp.find(size+1) == mp.end()) {
+			ans++;
+			size = mp.begin()->first;
+			if(--mp[size] == 0) mp.erase(size);
+			
+		} else {
+			if(--mp[size+1] == 0) mp.erase(size+1);
+			size++;
+		}
+	}
+	cout << ans+1 << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> T;
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
