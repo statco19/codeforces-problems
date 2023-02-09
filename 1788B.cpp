5345685f9472e03b611b5cf2b81aabcdf92d3d17
andrew <statco19@gmail.com>
2023-02-10 01:24:04 +0900

    math, impl 900

---
+++ 1788B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)((x).size())
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+
+void sol() {
+	int n;
+	cin >> n;
+	if(n & 1) {
+		int x = 0;
+		int y = 0;
+
+		int turn = 0;
+		int base = 1;
+		while(n) {
+			int k = n % 10;
+			if(k & 1) {
+				if(turn == 0) {
+					x += k/2 * base;
+					y += (k/2 + 1) * base;
+				} else {
+					x += (k/2 + 1) * base;
+					y += k/2 * base;
+				}
+				turn ^= 1;
+			} else {
+				x += k/2 * base;
+				y += k/2 * base;
+			}
+			
+			base *= 10;
+			n /= 10;
+		}
+
+		cout << x << " " << y << en;
+	} else {
+		cout << n/2 << " " << n/2 << en;
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> T;
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
