84d142157e964cef8f2a10a32da31eb468a05928
andrew <statco19@gmail.com>
2023-02-20 20:22:58 +0900

    greedy 800

---
+++ 1776A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)((x).size())
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n;
+	cin >> n;
+	vt<int> a(n);
+	for(int &x : a) cin >> x;
+
+	int t = 0;
+	int walk = 0;
+	for(int i=0; i<n; ++i) {
+		if(t+120 <= a[i]) {
+			walk += (a[i]-t)/120;
+		}
+		t = a[i];
+	}
+
+	walk += (1440-t)/120;
+	if(walk >= 2) cout << "YES" << en;
+	else cout << "NO" << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> T;
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
