6c2ae7c4417b1b7c650e2697fcd15d23b0233831
andrew <statco19@gmail.com>
2023-03-02 17:09:37 +0900

    math 1600 *

---
+++ 1796C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+#define vt vector
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)((x).size())
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define en '\n'
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+const int mod = 998244353;
+void sol() {
+	int l,r;
+	cin >> l >> r;
+
+	int m = 1;
+	while((l<<m) <= r) m++;
+
+	int ans = ((r / (1<<(m-1))) - l + 1);
+	if(m>1) ans = ans % mod + (max(0,(r / (1<<(m-2)) / 3 - l + 1)) * (m-1)) % mod;
+
+	cout << m << ' ' << ans % mod << en;
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> T;	
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
